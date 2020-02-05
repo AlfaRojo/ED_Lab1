0b5efc71e6491b564ddfa18a464c2cb8ac0d8543
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-02-05 09:30:33 -0600

    Arreglando nodo arbol

--- Lab1/Models/Node.cs
+++ Lab1/Models/Node.cs
@@ -3,34 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text;
+using System.Web;
 
 namespace Lab1.Models
 {
-	public class Node <T>
-	{
-        public Soda[] myInformation { get; set; }
-        public Node<T> nRight { get; set; }
-        public Node<T> nLeft { get; set; }
-        public Node<T> father { get; set; }
-        public bool root { get { return father == null; } }
-        public bool leaf { get { return nLeft == null && nRight == null; } }
+    public class Node<TKey, T>
+    {
+        public List<Soda> myInformation { get; set; }
+        public List<int> children { get; set; }
+        public List<TKey> key { get; set; }
+        public int father { get; set; }
+        public int order { get; set; }
+        public int position { get; set; }
 
-        public Node(Soda[] value, Node<T> father, Node<T> Left, Node<T> right)
+        public Node(List<Soda> Data, List<int> myChildren, List<TKey> myKey, int myFather, int myOrder, int myPositioin)
         {
-            myInformation = value;
-            this.nRight = right;
-            this.nLeft = Left;
-            this.father = father;
+            this.myInformation = Data;
+            this.children = myChildren;
+            this.key = myKey;
+            this.father = myFather;
+            this.position = myPositioin;
+            this.order = myOrder;
         }
-        public Node(Soda[] Value)
+        public Node()
         {
-            myInformation = Value;
+            myInformation = new List<Soda>();
+            children = new List<int>();
+            key = new List<TKey>();
         }
-        public Node(Soda[] Value, Node<T> Father)
-        {
-            myInformation = Value;
-            this.father = Father;
-        }
-
     }
 }
